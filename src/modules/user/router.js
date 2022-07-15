231d8e715830e9290588bf7bb638b81520d2b298
Littledropbootcamp <littledropedu@gmail.com>
2022-07-14 12:28:06 -1200

    test commit

--- src/modules/user/router.js
+++ src/modules/user/router.js
@@ -4,6 +4,9 @@ const { signin, signup } = require("./controller")
 const UserRouter = (app) => {
     Router.post('/signup', signup)
     Router.post('/signin', signin)
+    Router.get('/', (req, res) => {
+        res.send("API up and running")
+    })
     app.use(Router)
 }
 
